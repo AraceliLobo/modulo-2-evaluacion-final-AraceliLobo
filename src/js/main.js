76c93939dd5243d438feabf422f191996a546b80
Araceli <a.lobocasas@gmail.com>
2020-10-23 16:43:05 +0200

    Add images

--- src/js/main.js
+++ src/js/main.js
@@ -29,7 +29,9 @@ function getShows(ev) {
       paintSeries();
     });
 }
-
+paintSeries();
+// listenSeries();
+// paintFavorites();
 // Función para pintar series
 function paintSeries() {
   // Aquí hay que meter algo más??
@@ -44,7 +46,42 @@ function paintSeries() {
     seriesList.innerHTML += `</li>`;
   }
 }
+// // Función para pintar favoritas
+// function paintFavorites() {
+//   for (let i = 0; i < shows.length; i++) {
+//     let classF;
+//     const indexFav = favorites.indexOf(i);
+//     const isFavorite = indexFav !== -1;
+//     if (favorite === true) {
+//       classF = "pallettes_item--favorite";
+//     }
+//     else {
+//       classF = "";
+//     }
+//     favList.innerHTML += `<li class="js-palette-item palettes_item--favorite" id = "${i}">`;
+//     favList.innerHTML += `</li>`;
+// }
 
+// // Función para colocar las favoritas en su lista
+// const favoritesSeries = function (ev) {
+//   const clicked = parseInt(ev.currentTarget.id);
+//   // 4.7 para meter o quitar de favoritos (con un método de array indexOf)
+//   const indexFav = favorites.indexOf(clicked);
+//   const isFavorite = indexFav !== -1;
+//   if (isFavorite === false) {
+//     favorites.push(clicked);
+//   } else {
+//     favorites.splice(indexFav, 1);
+//   }
+// };
+
+// // Función para guardar en favoritas
+// function listenSeries() {
+//   const seriesItem = document.querySelectorAll(".js-palette-item");
+//   for (const serieItem of seriesItem) {
+//     serieItem.addEventListener("click", favoritesSeries);
+//   }
+// }
 // Escuchador en el botón de buscar (al hacer click conecta con la API)
 btnSearch.addEventListener("click", getShows);
 
