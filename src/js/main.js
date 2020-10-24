aa25276e58118ecca338f8bac3d15501e1ab87df
Araceli <a.lobocasas@gmail.com>
2020-10-24 17:13:39 +0200

    Add js favorites and styyles

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-unused-vars */
 "use strict";
-// Constantes definidas
+// 1. Constantes definidas
 const inputSearch = document.querySelector(".js-search");
 const seriesList = document.querySelector(".js-seriesList");
 const favList = document.querySelector(".js-favList");
 const btnSearch = document.querySelector(".js-btnSsearch");
 const btnRemove = document.querySelector(".js-btnRemoveAll");
 
-// Array para la lista de series
+// 2. Array para la lista de series
 let shows = [];
 
 // Array para la lista de favoritas
 let favorites = [];
 
-// Petición a la API
+let clickedSerie;
+listenSerie();
+
+// 3. Petición a la API
 function getShows(ev) {
   ev.preventDefault();
   const search = inputSearch.value;
@@ -25,65 +28,64 @@ function getShows(ev) {
       // Guardamos los datos recogidos que nos interesan en un nuevo array
       shows = data;
       console.log(data);
-      // Aquí llamamos a la función pintar series
+      // 6. Aquí llamamos a la función pintar series
       paintSeries();
     });
 }
-paintSeries();
-// listenSeries();
-// paintFavorites();
-// Función para pintar series
+// 5. Definimos función para que se pinten las series (creamos html)
 function paintSeries() {
-  // Aquí hay que meter algo más??
-  for (let show of shows) {
-    seriesList.innerHTML += `<li class="serie-item js-item">`;
-    if (show.show.image === null) {
-      seriesList.innerHTML += `<img src="./assets/images/noimage.png"/>`;
+  let html = "";
+  for (let i = 0; i < shows.length; i++) {
+    if (shows[i].show.image === null) {
+      html += `<li class="serie-item js-item" id="${[i]}">`;
+      html += `<h3 class= "serie-name js-name">${shows[i].show.name}</h3>`;
+      html += `<img class="serie-image js-image" src="./assets/images/noimage.png"/>`;
+      html += "</li>";
     } else {
-      seriesList.innerHTML += `<img class="serie-image js-image" src="${show.show.image.medium}"/>`;
+      html += `<li class="serie-item js-item" id="${[i]}">`;
+      html += `<h3 class="serie-name js-name">${shows[i].show.name}</h3>`;
+      html += `<img clas="serie-image js-image" src="${shows[i].show.image.medium}"/>`;
+      html += "</li>";
     }
-    seriesList.innerHTML += `<h3 class="serie-name js-name">${show.show.name}</h3>`;
-    seriesList.innerHTML += `</li>`;
+    seriesList.innerHTML = html;
   }
 }
-// // Función para pintar favoritas
-// function paintFavorites() {
-//   for (let i = 0; i < shows.length; i++) {
-//     let classF;
-//     const indexFav = favorites.indexOf(i);
-//     const isFavorite = indexFav !== -1;
-//     if (favorite === true) {
-//       classF = "pallettes_item--favorite";
-//     }
-//     else {
-//       classF = "";
-//     }
-//     favList.innerHTML += `<li class="js-palette-item palettes_item--favorite" id = "${i}">`;
-//     favList.innerHTML += `</li>`;
-// }
 
-// // Función para colocar las favoritas en su lista
-// const favoritesSeries = function (ev) {
-//   const clicked = parseInt(ev.currentTarget.id);
-//   // 4.7 para meter o quitar de favoritos (con un método de array indexOf)
-//   const indexFav = favorites.indexOf(clicked);
-//   const isFavorite = indexFav !== -1;
-//   if (isFavorite === false) {
-//     favorites.push(clicked);
-//   } else {
-//     favorites.splice(indexFav, 1);
-//   }
-// };
+// Función para añadir a favoritas
+function listenSerie() {
+  const items = document.querySelectorAll(".js-item");
+  for (const item of items) {
+    item.addEventListener("click", selectFav);
+  }
+}
+// Funciones para seleccionar favoritas
+function selectFav(event) {
+  clickedSerie = parseInt(event.currentTarget.id);
+  const isFav = favorites.indexOf(clickedSerie);
+  if (isFav === -1) {
+    favList.push(clickedSerie);
+    event.currentTarget.classList.add("fav-item");
+  } else if (isFav > -1) {
+    favorites.splice(isFav, 1);
+    event.currentTarget.classList.remove("fav-item");
+  }
+}
 
-// // Función para guardar en favoritas
-// function listenSeries() {
-//   const seriesItem = document.querySelectorAll(".js-palette-item");
-//   for (const serieItem of seriesItem) {
-//     serieItem.addEventListener("click", favoritesSeries);
-//   }
-// }
-// Escuchador en el botón de buscar (al hacer click conecta con la API)
+// Función para pintar favoritas
+let favHtml = "";
+function paintFav() {
+  favHtml += `<li class="fav-item js-favItem" id="${clickedSerie}">`;
+  favHtml += `<h3 class="favCard__title">${shows[clickedSerie].show.name}</h3>`;
+  if (shows[clickedSerie].show.image === null) {
+    favHtml += `<img class="fav-image js-favImage" src="./assets/images/noimage.png"/>`;
+  } else {
+    favHtml += `<img class="fav-image js-favImage" src="${shows[clickedSerie].show.image.medium}"/>`;
+  }
+  favHtml += "</li>";
+  favList.innerHTML = favHtml;
+}
+// 4. Escuchador en el botón de buscar (al hacer click conecta con la API)
 btnSearch.addEventListener("click", getShows);
 
 // Simulador de click - lo quitamos al final
-btnSearch.click();
+// btnSearch.click();
