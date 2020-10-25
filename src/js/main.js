351e1eb232ba336ba562eb46b3fddea3032ddd9a
Araceli <a.lobocasas@gmail.com>
2020-10-25 18:28:39 +0100

    Function add favorites

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@ const btnRemove = document.querySelector(".js-btnRemoveAll");
 
 // 2. Array para la lista de series
 let shows = [];
-
 // Array para la lista de favoritas
 let favorites = [];
 
@@ -24,10 +23,10 @@ function getShows(ev) {
     .then(function (data) {
       // Guardamos los datos recogidos que nos interesan en un nuevo array
       shows = data;
-      console.log(data);
       // 6. Aquí llamamos a la función pintar series
       paintSeries();
       listenSeries();
+      paintFavorites();
     });
 }
 // 5. Definimos función para que se pinten las series (creamos html) además añadimos la etiqueta da favorita
@@ -54,6 +53,7 @@ function paintSeries() {
     seriesList.innerHTML = html;
   }
 }
+// Función para que se marquen como favoritas en la lista de series
 function favoritesSeries(ev) {
   const clicked = parseInt(ev.currentTarget.id);
   const indexFav = favorites.findIndex(function (show, index) {
@@ -72,14 +72,38 @@ function favoritesSeries(ev) {
   paintSeries();
   listenSeries();
 }
-
+// Función para escuchar cuando hacemos click en una serie
 function listenSeries() {
   const series = document.querySelectorAll(".js-item");
   for (const serie of series) {
     serie.addEventListener("click", favoritesSeries);
   }
 }
+// Función para que las seleccionadas aparezcan en la lista de favoritas
+function paintFavorites() {
+  let favHtml = "";
+  for (let i = 0; i < shows.length; i++) {
+    let classF = "";
+    const favoriteIndex = favorites.findIndex(function (show, index) {
+      return show.id === shows[i].show.id;
+    });
+    if (favoriteIndex !== -1) {
+      classF = "fav-item";
+    } else {
+      classF = "";
+    }
+    favHtml += `<li class="fav-item" id="${shows[i].show.id}">`;
+    favHtml += `<h3 class="fav-name">${shows[i].show.name}</h3>`;
+    if (shows[i].show.image === null) {
+      favHtml += `<img class="fav-image" src="./assets/images/noimage.png"/>`;
+    } else {
+      favHtml += `<img class="fav-image" src="${shows[i].show.image.medium}"/>`;
+    }
+    favHtml += "</li>";
 
+    favList.innerHTML = favHtml;
+  }
+}
 // 4. Escuchador en el botón de buscar (al hacer click conecta con la API)
 btnSearch.addEventListener("click", getShows);
 
